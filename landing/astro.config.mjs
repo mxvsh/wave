0bc8121a7d4882ec03c245b0b0a3c93ed14b8f0f
mxvsh <hello@monawwar.io>
2026-03-30 02:42:22 +0530

    chore(web): setup sitemap

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: "https://wave.mxv.sh",
+  integrations: [sitemap()]
 });
