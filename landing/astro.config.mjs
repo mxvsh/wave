485faba6f525e242f42d9f805c8bfacb8617cabb
mxvsh <hello@monawwar.io>
2026-03-30 09:55:49 +0530

    chore(web): add selection mode section

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
-
 import sitemap from "@astrojs/sitemap";
-
-
-import cloudflare from "@astrojs/cloudflare";
 
 
 // https://astro.build/config
@@ -14,8 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   site: "https://wave.mxv.sh",
-  integrations: [sitemap({ filter: (page) => !page.includes("/og-banner") })],
-  adapter: cloudflare()
+  integrations: [sitemap({ filter: (page) => !page.includes("/og-banner") })]
 });
